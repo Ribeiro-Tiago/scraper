fcbfe867098330ae3f3653c52afce7c719464b91
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2020-04-19 19:09:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Carga de trabalhos scrapper
+
+A scrapper that works as a sort of job alert for http://www.cargadetrabalhos.net.
---
+++ functions/types/index.ts
@@ -0,0 +1,6 @@
+export interface ResultObj {
+    title: string;
+    contractType: string;
+    local: string;
+    link: string;
+};

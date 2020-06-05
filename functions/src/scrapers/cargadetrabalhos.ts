98e814d260c6a9bd2f69dd276b668879f0f36a99
Tiago Ribeiro <tiago@accountable.com>
2020-06-05 20:16:11 +0200

    updated cargadetrabalhos to use last post instead of time

--- functions/src/scrapers/cargadetrabalhos.ts
+++ functions/src/scrapers/cargadetrabalhos.ts
@@ -2,32 +2,7 @@ import fetch from "node-fetch";
 import { JSDOM } from "jsdom";
 
 import { sendEmail } from "../utils/mailer";
-import { DateValidator } from "../types/cargadetrabalhos";
-
-// hashmap to get the current-date.getMonth()-compatible month "number"
-// according to the specified month name in portuguese
-const months = new Map([
-	["Janeiro", 0],
-	["Fevereiro", 1],
-	["Março", 2],
-	["Abril", 3],
-	["Maio", 4],
-	["Junho", 5],
-	["Julho", 6],
-	["Agosto", 7],
-	["Setembro", 8],
-	["Outubro", 9],
-	["Novembro", 10],
-	["Dezembro", 11],
-]);
-
-const isNewPost = ({ post, today }: DateValidator): boolean => {
-	const getMonthNumber = months.get(post.month);
-
-	return today.month === getMonthNumber
-		? parseInt(post.day) === today.day
-		: false;
-};
+import { getLastPost, updateLastPost } from "../firestore/cargadetrabalhos";
 
 const url =
 	"http://www.cargadetrabalhos.net:80/category/web-design-programacao?s=+freelance";
@@ -35,15 +10,6 @@ const url =
 export default async () => {
 	try {
 		const result = await (await fetch(url)).text();
-
-		/* today's date variables, to avoid having to define them every single iteration */
-		const d = new Date();
-		const today = {
-			day: d.getDate(),
-			month: d.getMonth(),
-		};
-
-		let postDate: string[];
 		let link: string;
 		let scraped = "";
 		let elem;
@@ -52,26 +18,19 @@ export default async () => {
 			".entrycontent",
 		);
 
+		const lastPostLink = await getLastPost();
+
 		for (elem of elems) {
-			postDate = elem.querySelector(".date").textContent.split(" / ");
-
-			if (
-				!isNewPost({
-					today,
-					post: {
-						day: postDate[0],
-						month: postDate[1],
-					},
-				})
-			) {
-				break;
-			}
-
 			link = elem
 				.querySelector(".pie")
 				.querySelector("a")
 				.getAttribute("href")
 				.split("/print")[0];
+
+			if (lastPostLink === link) {
+				await updateLastPost(link);
+				break;
+			}
 
 			scraped += `<a href="${link}"><h2>${
 				elem.querySelector("h2").textContent

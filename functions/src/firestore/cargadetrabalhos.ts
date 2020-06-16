e5d104c6c9dc08d63ece9501b267b64d5b9b4938
ribeiro-tiago <tiagoribeiro.1803@gmail.com>
2020-06-16 13:05:35 +0200

    updated db structure to make it more scalable & scripts

--- functions/src/firestore/cargadetrabalhos.ts
+++ functions/src/firestore/cargadetrabalhos.ts
@@ -1,11 +1,12 @@
 import { getLastEntry, updateLastEntry } from "./utils";
+import { Scrapers } from "../types";
 
 export const getLastPost = async () => {
-	const post = await getLastEntry("cargadetrabalhos");
+	const post = await getLastEntry(Scrapers.CARGA_DE_TRABALHOS);
 
 	return !!post ? post.link : null;
 };
 
 export const updateLastPost = async (link: string) => {
-	return updateLastEntry("cargadetrabalhos", { link });
+	return updateLastEntry(Scrapers.CARGA_DE_TRABALHOS, { link });
 };
--- functions/src/firestore/utils.ts
+++ functions/src/firestore/utils.ts
@@ -1,16 +1,16 @@
 import { db } from "../configs/index";
 import { Scrapers } from "../types";
 
-type Collection = "cargadetrabalhos" | "recipients";
+type Collection = "scrapers" | "recipients";
 
 const collection = (collection: Collection) => db.collection(collection);
 
-export const getLastEntry = async (collName: Collection) => {
-	return (await collection(collName).doc("entries").get()).data();
+export const getLastEntry = async (scraper: Scrapers) => {
+	return (await collection("scrapers").doc(scraper).get()).data();
 };
 
-export const updateLastEntry = async (collName: Collection, data: object) => {
-	return await collection(collName).doc("entries").set(data);
+export const updateLastEntry = async (scraper: Scrapers, data: object) => {
+	return await collection("scrapers").doc(scraper).set(data);
 };
 
 export const getRecipients = async (scraper: Scrapers) => {

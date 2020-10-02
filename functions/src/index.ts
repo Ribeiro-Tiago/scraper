1eb79e1d24414543adfa371fd26b7f34673dc340
Tiago Ribeiro <t.ribeiro@sweepatic>
2020-10-02 11:17:07 +0200

    updated function region to US (supposedly it's free in US)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,10 +4,8 @@ import * as functions from "firebase-functions";
 import CDT from "./scrapers/cargadetrabalhos";
 
 export const cargaDeTrabalhos = functions
-	.region("europe-west1")
 	.pubsub.schedule("0 0 * * *")
 	.onRun(() => CDT());
 
 export const cargaDeTrabalhosRequest = functions
-	.region("europe-west1")
 	.https.onRequest(() => CDT());

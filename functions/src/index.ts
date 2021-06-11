ebf3eeb85c8387dd6a5fd799639f985176bd8c72
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2021-06-11 09:47:16 +0200

    commented out CDT

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,12 @@
 import "./firestore/utils";
 import * as functions from "firebase-functions";
 
-import CDT from "./scrapers/cargadetrabalhos";
+// import CDT from "./scrapers/cargadetrabalhos";
 import ELF from "./scrapers/elf";
 
-export const cargaDeTrabalhos = functions.pubsub
-	.schedule("0 0 * * *")
-	.onRun(() => CDT());
+// export const cargaDeTrabalhos = functions.pubsub
+// 	.schedule("0 0 * * *")
+// 	.onRun(() => CDT());
 
 export const elf = functions.pubsub.schedule("0 0 * * *").onRun(() => ELF());
 

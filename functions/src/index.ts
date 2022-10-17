6717b44f2ae22a0a23c793855556fb2015d12102
Tiago Ribeiro <t.ribeiro@sweepatic.com>
2022-10-17 15:30:35 +0100

    reenable CDT

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,14 @@
 import "./firestore/utils";
 import * as functions from "firebase-functions";
 
-// import CDT from "./scrapers/cargadetrabalhos";
-import ELF from "./scrapers/elf";
+import CDT from "./scrapers/cargadetrabalhos";
+// import ELF from "./scrapers/elf";
 
-// export const cargaDeTrabalhos = functions.pubsub
-// 	.schedule("0 0 * * *")
-// 	.onRun(() => CDT());
+export const cargaDeTrabalhos = functions.pubsub
+	.schedule("0 0 * * *")
+	.onRun(CDT);
 
-export const elf = functions.pubsub.schedule("0 0 * * *").onRun(() => ELF());
+// export const elf = functions.pubsub.schedule("0 0 * * *").onRun(() => ELF());
 
 // used for testing locally
 // export const cargaDeTrabalhosRequest = functions.https.onRequest(() => CDT());

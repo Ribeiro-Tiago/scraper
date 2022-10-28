603124438260da49524abdd5fd268c545422bd73
Tiago Ribeiro <t.ribeiro@sweepatic.com>
2022-10-28 12:02:21 +0100

    add check for date as well

--- functions/src/firestore/cargadetrabalhos.ts
+++ functions/src/firestore/cargadetrabalhos.ts
@@ -1,12 +1,24 @@
+import dayjs from "dayjs";
+
 import { getLastEntry, updateLastEntry } from "./utils";
 import { Scrapers } from "../types";
 
 export const getLastPost = async () => {
 	const post = await getLastEntry(Scrapers.CARGA_DE_TRABALHOS);
 
-	return !!post ? post.link : null;
+	if (!post) {
+		return { link: null, updated: null };
+	}
+
+	return {
+		link: post.link,
+		updated: dayjs(post.updated ?? Date.now()),
+	};
 };
 
 export const updateLastPost = async (link: string) => {
-	return updateLastEntry(Scrapers.CARGA_DE_TRABALHOS, { link });
+	return updateLastEntry(Scrapers.CARGA_DE_TRABALHOS, {
+		link,
+		updated: Date.now(),
+	});
 };
--- functions/src/scrapers/cargadetrabalhos.ts
+++ functions/src/scrapers/cargadetrabalhos.ts
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import { JSDOM } from "jsdom";
+import dayjs, { Dayjs } from "dayjs";
 
 import { emailRecipients, emailAdmin } from "../utils/mailer";
 import { getLastPost, updateLastPost } from "../firestore/cargadetrabalhos";
@@ -8,6 +9,27 @@ import { Scrapers } from "../types";
 const url =
 	"http://www.cargadetrabalhos.net:80/category/web-design-programacao?s=+freelance";
 
+const MONTH_MAPPER = {
+	Janeiro: 1,
+	Fevereiro: 2,
+	Março: 3,
+	Abril: 4,
+	Maio: 5,
+	Junho: 6,
+	Julho: 7,
+	Agosto: 8,
+	Setembro: 9,
+	Outubro: 10,
+	Novembro: 11,
+	Dezembro: 12,
+} as { [month: string]: number };
+
+const parseDate = (str: string): Dayjs => {
+	const [day, month, year] = str.split(" / ");
+
+	return dayjs(`${year}-${MONTH_MAPPER[month]}-${day}`);
+};
+
 export default async () => {
 	try {
 		const result = await (await fetch(url)).text();
@@ -15,25 +37,26 @@ export default async () => {
 		let scraped = "";
 		let elem: Element;
 		let firstResultLink: string;
+		let pie: Element;
+		let date: Dayjs;
 
 		const elems = new JSDOM(result).window.document.querySelectorAll(
 			".entrycontent",
 		);
 
-		const lastPostLink = await getLastPost();
+		const { link: lastPostLink, updated } = await getLastPost();
 
 		for (elem of elems) {
-			link = elem
-				.querySelector(".pie")
-				.querySelector("a")
-				.getAttribute("href")
-				.split("/print")[0];
+			pie = elem.querySelector(".pie");
+			link = pie.querySelector("a").getAttribute("href").split("/print")[0];
+
+			date = parseDate(pie.querySelector(".date").textContent);
 
 			if (!firstResultLink) {
 				firstResultLink = link;
 			}
 
-			if (lastPostLink === link) {
+			if (lastPostLink === link || date.isBefore(updated, "day")) {
 				await updateLastPost(firstResultLink);
 				break;
 			}
@@ -53,7 +76,7 @@ export default async () => {
 			scraped,
 		);
 	} catch (err) {
-		await emailAdmin("Scrapper error - carga de trabalhos", err);
 		console.error("err during scrap", err);
+		await emailAdmin("Scrapper error - carga de trabalhos", err as string);
 	}
 };
